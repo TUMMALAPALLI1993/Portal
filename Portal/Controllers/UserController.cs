fba15a7ac99c6aace702a59fe0fab7090400df2c
Kiran93 <tummalapalli.kirankumar@gmail.com>
2019-08-15 10:31:47 +0200

    Database and registration page created

---
+++ Portal/Controllers/UserController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Portal.Controllers
+{
+    using Portal.Models;
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult CreateOrEditUser(int id=0)
+        {
+            User user = new User();
+            return View(user);
+        }
+
+        [HttpPost]
+        public ActionResult CreateOrEditUser(User user)
+        {
+            using(DBmodels db=new DBmodels())
+            {
+                if (db.Users.Any(x => user.UserName == x.UserName))
+                {
+                    ViewBag.DuplicateMessage = "User Name '" + user.UserName + "' already exists";
+                }
+                else
+                {
+                    db.Users.Add(user);
+                    db.SaveChanges();
+                }
+                
+            }
+            ViewBag.successmessage = "Successfully " + user.UserName + " added to the portal";
+            ModelState.Clear();
+            return View("CreateOrEditUser",new User());
+        }
+    }
+}
